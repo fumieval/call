6c9372faf57beaf3c2448620bf20aa98d91246b9
Fumiaki Kinoshita <fumiexcel@gmail.com>
2014-09-20 11:59:21 +0900

    Add utilities

---
+++ src/Call/Util.hs
@@ -0,0 +1,28 @@
+module Call.Util where
+import Control.Monad.Objective.Class
+import Control.Object
+import Call.Component
+import Call.Component.Deck
+import Call.System
+import Call.Types
+import Call.Data.Wave
+import Call.Picture
+import Control.Lens
+
+animate :: (Time -> System s (Picture ())) -> System s (Address PullGraphic (System s))
+animate f = do
+  p <- invoke (go 0)
+  linkGraphic p
+  return p
+  where
+    go t = Object $ \(PullGraphic dt cont) -> f t >>= \p -> return (cont p, go (t + dt))
+
+withSound :: Source Stereo -> System s a -> System s a
+withSound src m = do
+  deck <- invoke emptyDeck
+  deck .& source ?= src
+  deck .& playing .= True
+  linkAudio deck
+  r <- m
+  unlinkAudio deck
+  return r
