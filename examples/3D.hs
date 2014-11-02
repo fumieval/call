f93fab8b83ec8e9fe0b270e0c82a112eea59fa49
Fumiaki Kinoshita <fumiexcel@gmail.com>
2014-11-02 14:36:22 +0900

    use my own API

--- examples/3D.hs
+++ examples/3D.hs
@@ -1,9 +1,6 @@
 import Call
 import Control.Lens
-import Control.Monad.Objective
 import qualified Data.Vector.Storable as V
-import Foreign.C (CFloat)
-import Call.Data.Bitmap (Bitmap(..))
 import System.IO.Unsafe
 import Data.Monoid
 bmp = unsafePerformIO $ readBitmap "examples/stonebrick.png"
@@ -15,7 +12,7 @@ block = v0 <> v1 <> v2 where
     , V3 0 0 1 `Vertex` V2 0 1
     , V3 0 1 0 `Vertex` V2 1 0
     , V3 0 1 1 `Vertex` V2 1 1
-    , V3 1 1 0 `Vertex` V2 1 0
+    , V3 1 1 0 `Vertex` V2 0 0
     , V3 1 1 1 `Vertex` V2 0 1
     ]
   v1 = vertices bmp $ V.fromList [
@@ -35,13 +32,7 @@ block = v0 <> v1 <> v2 where
     , V3 1 1 1 `Vertex` V2 0 1
     ]
 
-transformation :: (Epsilon a, Floating a) => a -> M44 a
-transformation = liftA2 (!*!) b a where
-  a t = m33_to_m44 $ fromQuaternion $ axisAngle (V3 0 1 0) (t * 2)
-  b t = eye4 & translation .~ V3 (sin t * 2) (cos (t / 4) * 2) (-5)
-
 main = runSystemDefault $ do
-    obj <- new $ animate $ \t -> projectPicture (perspective 120 (4/3) 1 1000)
-        $ transformPicture (transformation t) block
-    linkGraphic obj
-    stand
+  newGraphic $ animate $ \t -> projectPicture (perspective 120 (4/3) 1 1000)
+      $ translate (V3 (sin t * 2) (cos (t / 4) * 2) (-5)) $ rotateOn (V3 0 (t / 2) 0) block
+  stand
