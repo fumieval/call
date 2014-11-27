de9eda399a5a47355a1bb4b690017f74374bb678
Fumiaki Kinoshita <fumiexcel@gmail.com>
2014-11-27 15:34:37 +0900

    it's typewriter

--- src/Call/Util/Text.hs
+++ src/Call/Util/Text.hs
@@ -3,23 +3,23 @@ module Call.Util.Text where
 import Control.Lens
 import Data.Functor.Request
 import Data.Functor.PushPull
-import Control.Monad.Objective.Class
 import Control.Object
 import Linear
 import Call.Data.Bitmap (Bitmap)
 import Call.Sight
 import Data.Monoid
 import Control.Monad.State.Class
-import Control.Elevator
 import Control.Monad.Trans
+import Call.Data.Font
 
-type RenderChar = Request Char (Bitmap, V2 Float, V2 Float)
+renderer :: MonadIO m => Font -> Float -> Object (Request Char (Bitmap, V2 Float, V2 Float)) m
+renderer font size = flyweight (liftIO . renderChar font size)
 
-typewriter :: (MonadObjective m, Tower m) => Instance' RenderChar m -> Object (PushPull Char Picture) m
-typewriter fon = stateful go (V2 0 0, mempty) where
+typewriter :: MonadIO m => (Char -> m (Bitmap, V2 Float, V2 Float)) -> Object (PushPull Char Picture) m
+typewriter req = stateful go (V2 0 0, mempty) where
   go (Push ch cont) = do
     (pos, pic) <- get
-    (bmp, ofs, adv) <- lift $ fon .- request ch
+    (bmp, ofs, adv) <- lift $ req ch
     put (pos + adv, translate (pos + ofs) (bitmap bmp) <> pic)
     return cont
   go (Pull cont) = uses _2 cont
